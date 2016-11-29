4604c01598685a25e4afefe08ab3f4410369aae8
Augustus175 <columbia7311@163.com>
2016-11-29 23:42:40 +0800

    2016-11-29 23：43

--- src/PAT_1012.java
+++ src/PAT_1012.java
@@ -1,7 +1,11 @@
 /**
  * Created by Gavin.Stevenson on 2016/11/29.
  */
+import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PAT_1012 {
     public static void main(String[] args)
     {
@@ -13,15 +17,43 @@ public class PAT_1012 {
 //        }
         int[] num = new int[numstr.length];
         int even = 0;
-        int[] numA2 = null;
+        int numA2 = 0;
+        int A2count = 0;
+        int A3count = 0;
+        int numA4 = 0;
+        int A4count = 0;
+        int numA5 = 0;
         for (int i = 0; i < numstr.length; i++) {
             num[i] = Integer.parseInt(numstr[i]);
             if((num[i] % 5 == 0)&&(num[i] % 2 == 0))
             {
                 even = even + num[i];
-            }else if () {
+            }else if (num[i] % 5 == 1) {
+                A2count++;
+                if (A2count % 2 == 0) {
+                    numA2 = num[i] + numA2;
+                }else
+                {
+                    numA2 = num[i] +(-1) * numA2;
+                }
+            }else if(num[i] % 5 == 2)
+            {
+                A3count++;
+            }else if (num[i] % 5 == 3)
+            {
+                A4count++;
+                numA4 = numA4 + num[i];
+            }else if (num[i] % 5 == 4)
+            {
+                if (numA5 < num[i])
+                {
+                    numA5 = num[i];
+                }
             }
         }
+        DecimalFormat df = new DecimalFormat("0.0");
+        double res = (double)numA4/A4count;
+        String ave = df.format(res);
         if()
     }
 }
