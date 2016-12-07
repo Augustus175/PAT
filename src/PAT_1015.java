cffdd9a99431dc1b9e756bf7fb3985d324e344dc
Augustus175 <columbia7311@163.com>
2016-12-07 09:28:31 +0800

    2016-12-07 09:22

---
+++ src/PAT_1015.java
@@ -0,0 +1,30 @@
+/**
+ * Created by thinkpad on 2016/12/3.
+ */
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+public class PAT_1015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Num = sc.nextInt();
+        int Low = sc.nextInt();
+        int High = sc.nextInt();
+        int[] stuID = new int[Num];
+        int[] moral = new int[Num];
+        int[] talent = new int[Num];
+        int[] total = new int[Num];
+        Map<Integer, String> map =new  HashMap<String, String>();
+//        Map<Integer,Integer> moral = new HashMap<Integer,Integer>();
+//        Map<Integer,Integer> talent = new HashMap<Integer,Integer>();
+//        Map<Integer,Integer> total = new HashMap<Integer,Integer>();
+        for (int i = 0; i < Num; i++) {
+            int id = sc.nextInt();
+            int m = sc.nextInt();
+            int ta = sc.nextInt();
+            int to = sc.nextInt();
+            map.put()
+        }
+
+    }
+}
