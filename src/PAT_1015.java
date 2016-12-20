bbc45f5864ee91a61b23c1302557d596dd365075
Augustus175 <columbia7311@163.com>
2016-12-20 23:40:34 +0800

    2016-12-20 23:42

--- src/PAT_1015.java
+++ src/PAT_1015.java
@@ -7,35 +7,55 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class PAT_1015 {
+    //        Map<Integer, String> map =new  HashMap<String, String>();
+    public static Map<Integer, Integer> moral = new HashMap<Integer, Integer>();
+    public static Map<Integer, Integer> talent = new HashMap<Integer, Integer>();
+    public static Map<Integer, Integer> total = new HashMap<Integer, Integer>();
+    public static int Low = 0;
+    public static int High = 0;
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int Num = sc.nextInt();
-        int Low = sc.nextInt();
-        int High = sc.nextInt();
+        Low = sc.nextInt();
+        High = sc.nextInt();
         int[] stuID = new int[Num];
-        int[] moral = new int[Num];
-        int[] talent = new int[Num];
-        int[] total = new int[Num];
-//        Map<Integer, String> map =new  HashMap<String, String>();
-//        Map<Integer,Integer> moral = new HashMap<Integer,Integer>();
-//        Map<Integer,Integer> talent = new HashMap<Integer,Integer>();
-//        Map<Integer,Integer> total = new HashMap<Integer,Integer>();
+//        int[] moral = new int[Num];
+//        int[] talent = new int[Num];
+//        int[] total = new int[Num];
+
         for (int i = 0; i < Num; i++) {
             int id = sc.nextInt();
             int m = sc.nextInt();
             int ta = sc.nextInt();
             int to = m + ta;
+
+            moral.put(id, m);
+            talent.put(id, ta);
+            total.put(id, to);
             stuID[i] = id;
-            moral[i] = m;
-            talent[i] = ta;
-            total[i] = to;
+//            moral[i] = m;
+//            talent[i] = ta;
+//            total[i] = to;
 //            map.put()
         }
-        for (int i = 0; i < Num; i++) {
-            System.out.println(stuID[i]);
-            System.out.println(moral[i]);
-            System.out.println(talent[i]);
-            System.out.println(total[i]);
+//        for (int i = 0; i < Num; i++) {
+//            System.out.println(stuID[i]);
+//            System.out.println(moral[i]);
+//            System.out.println(talent[i]);
+//            System.out.println(total[i]);
+//        }
+
+    }
+
+    public static int compare(int i, int j) {
+        if (moral.get(i) >= Low && talent.get(i) >= Low)
+        {
+
+        }else
+        {
+            return -1;
         }
 
     }
