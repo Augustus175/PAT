d6d748db704bed97363f60c2f94de897f3764558
Augustus175 <columbia7311@163.com>
2016-12-26 21:48:00 +0800

    2016-12-26 21:47

---
+++ src/PAT_1016.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Created by Gavin.Stevenson on 2016/12/26.
+ */
+public class PAT_1016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int DA = sc.nextInt();
+        int B = sc.nextInt();
+        int DB = sc.nextInt();
+        System.out.println(A);
+        System.out.println(DA);
+        System.out.println(B);
+        System.out.println(DB);
+    }
+    public static int newNum(String a, int da)
+    {
+
+        return 1;
+    }
+}
