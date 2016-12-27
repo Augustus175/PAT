77069a39d660f089edc5975cdb0ed8f534185477
Augustus175 <columbia7311@163.com>
2016-12-27 08:55:59 +0800

    2016-12-27 08:55

--- src/PAT_1016.java
+++ src/PAT_1016.java
@@ -10,35 +10,25 @@ public class PAT_1016 {
         int DA = sc.nextInt();
         int B = sc.nextInt();
         int DB = sc.nextInt();
-        System.out.println(A);
-        System.out.println(DA);
-        System.out.println(B);
-        System.out.println(DB);
-        newNum(A, 1);
+        int a = newNum(A, DA);
+        int b = newNum(B, DB);
+        System.out.println(a + b);
     }
-
     public static int newNum(int a, int da) {
-        int newnum = 0;
+        boolean flag = false;
+        int newnum = 1;
         for (int i = a; i != 0; i = i / 10) {
             int j = i % 10;
             if (j == da) {
-                if (newnum == 0) {
-                    newnum = 1;
-                } else {
-                    newnum = newnum * 10;
-                }
+                flag = true;
+                newnum = newnum * 10;
             }
-            System.out.println(j);
         }
-        if (newnum != 0) {
+        if (flag) {
             newnum = da * ((newnum - 1) / 9);
-            System.out.println("-----------------------------------------");
-            System.out.println(newnum);
             return newnum;
         } else {
-            return -1;
+            return 0;
         }
-
-
     }
 }
