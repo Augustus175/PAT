a66c0e802a95e9fd846ee3860090bc23df8c59ae
Augustus175 <columbia7311@163.com>
2016-12-27 23:43:03 +0800

    2016-12-27 23:45

---
+++ src/PAT_1017.java
@@ -0,0 +1,15 @@
+
+import java.math.BigInteger;
+import java.util.Scanner;
+/**
+ * Created by thinkpad on 2016/12/27.
+ */
+public class PAT_1017 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A =sc.nextBigInteger();
+        int B = sc.nextInt();
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
