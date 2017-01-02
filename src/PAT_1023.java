30182ba74012b56ce15b1282ef67e689c88ffec3
Augustus175 <columbia7311@163.com>
2017-01-02 10:33:17 +0800

    2017-01-02 10:33

--- src/PAT_1023.java
+++ src/PAT_1023.java
@@ -9,8 +9,30 @@ public class PAT_1023 {
         for (int i = 0; i < 10; i++) {
             num[i] = sc.nextInt();
         }
-        for (int i = 0; i < num.length; i++) {
-            System.out.println(num[i]);
+//        for (int i = 0; i < num.length; i++) {
+//            System.out.println(num[i]);
+//        }
+        StringBuffer sb = new StringBuffer();
+        int k = 1;
+        while(true)
+        {
+            if(num[k] != 0)
+            {
+                sb.append(k);
+                num[k]--;
+                break;
+            }else{
+                k++;
+            }
         }
+        for (int i = 0; i < 10; i++) {
+            if(num[i] != 0)
+            {
+                for (int j = 0; j < num[i]; j++) {
+                    sb.append(i);
+                }
+            }
+        }
+        System.out.println(sb);
     }
 }
