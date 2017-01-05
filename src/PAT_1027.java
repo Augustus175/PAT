8666f788937f1ce45929af08f24788a35ba5569b
Augustus175 <columbia7311@163.com>
2017-01-05 10:34:21 +0800

    2017-01-05 09:05

--- src/PAT_1027.java
+++ src/PAT_1027.java
@@ -19,20 +19,54 @@ public class PAT_1027 {
                 n++;
             }
             count = count - 4;
+            System.out.println(n);
+            int m = n - 1;
+            int counttmp1 = count;
+            System.out.println(count);
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < (count/2 - counttmp1 / 2 ) / 2; k++) {
+                    System.out.print(" ");
+                }
+                for (int k = counttmp1 / 2; k > 0; k--) {
+                    System.out.print(str);
+                }
+                System.out.println();
+                counttmp1 = counttmp1 - 4;
+            }
+//            -------------------------------------------------------------------------------------------------------------------------------------
+            for (int j = 0; j < (count-1 )/ 2 - 3; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("***");
+//            ---------------------------------------------------------------------------------------------------------------------------
+            for (int j = 0; j < (count-1)/2-2; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+
+            for (int j = 0; j < (count-1)/2-3; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("***");
+//            for (int j = 0; j < count / 2 - 2; j++) {
+//                System.out.print(" ");
+//            }
+            int counttmp2 = count;
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < j ; k++) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k <=counttmp2 / 2 ; k++) {
+                    System.out.print(str);
+                }
+                System.out.println();
+                counttmp2 = counttmp2 - 4;
+            }
+
         } else {
             System.out.println(str);
         }
 //        System.out.println(num + "  " + str);
-        System.out.println(n);
-        int m = n - 1;
-        int counttmp = count;
-        System.out.println(count);
-        for (int j = 0; j < m; j++) {
-            for (int k = counttmp / 2; k > 0; k--) {
-                System.out.print(str);
-            }
-            System.out.println();
-            counttmp = counttmp - 4;
-        }
+
     }
 }
