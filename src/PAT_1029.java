e2121cd72ee34a264cbe57333e4b4323369037aa
Augustus175 <columbia7311@163.com>
2017-01-09 22:30:05 +0800

    2017-01-09 22:30

---
+++ src/PAT_1029.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+/**
+ * Created by Gavin.Stevenson on 2017/1/9.
+ */
+public class PAT_1029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        List<Character> char1 = new ArrayList<Character>();
+        for (int i = 0; i < str1.length(); i++) {
+            char c = str1.charAt(i);
+            if ((c >= 97) && (c <= 122)) {
+                c = (char) (c - 32);
+            }
+            char1.add(c);
+        }
+
+        for (int j = 0; j < str2.length(); j++) {
+            for (int i = 0, len = char1.size(); i < len; ++i) {
+                char c = str2.charAt(j);
+                if ((c >= 97) && (c <= 122)) {
+                    c = (char) (c - 32);
+                }
+                if (char1.get(i) == c) {
+                    char1.remove(i);
+                    --len;//减少一个
+                    --i;//多谢deny_guoshou指正，如果不加会出现评论1楼所说的情况。
+                }
+            }
+        }
+        List newlist = removeDuplicateWithOrder(char1);
+        for (int i = 0; i < newlist.size(); i++) {
+            System.out.print(newlist.get(i));
+        }
+    }
+
+    public static List removeDuplicateWithOrder(List list) {
+        Set set = new HashSet();
+        List newList = new ArrayList();
+        for (Iterator iter = list.iterator(); iter.hasNext(); ) {
+            Object element = iter.next();
+            if (set.add(element))
+                newList.add(element);
+        }
+        list.clear();
+        list.addAll(newList);
+        return list;
+    }
+}
