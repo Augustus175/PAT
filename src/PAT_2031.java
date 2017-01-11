47f6cd510d1fd5465842a02d2a11fa8b65852dcf
Augustus175 <columbia7311@163.com>
2017-01-11 09:59:58 +0800

    2017-01-11 10:00

---
+++ src/PAT_2031.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+/**
+ * Created by Gavin.Stevenson on 2017/1/11.
+ */
+public class PAT_2031 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String[] str = new String[num];
+        boolean[] flag = new boolean[num];
+        for (int i = 0; i < num; i++) {
+            flag[i] = true;
+        }
+        int[] weight = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+        char[] M = {49,48,88,57,56,55,54,53,52,51,50};
+        for (int i = 0; i < num; i++) {
+            str[i] = sc.next();
+        }
+        for (int i = 0; i < num; i++) {
+            char[] ch = new char[18];
+            for (int j = 0; j < 17; j++) {
+                char c = str[i].charAt(j);
+                if ((c >= 48) && (c <= 57)) {
+                    ch[j] = c;
+                } else {
+                    flag[i] = false;
+                }
+            }
+            ch[17]=str[i].charAt(17);
+            if (flag[i]) {
+                int and = 0;
+                for (int j = 0; j < 17; j++) {
+                    and =and + (int) (ch[j] - 48) * weight[j];
+                }
+                int rem = and % 11;
+                if(M[rem]!=ch[17])
+                {
+                    flag[i] = false;
+                }
+
+            }
+        }
+        boolean flagAll = true;
+        for (int i = 0; i < num; i++) {
+            if(!flag[i])
+            {
+                System.out.println(str[i]);
+                flagAll = false;
+            }
+        }
+        if(flagAll)
+        {
+            System.out.print("All passed");
+        }
+
+    }
+}
