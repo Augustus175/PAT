160c418fe1ad25c3083f8968bd693a09e921b973
Augustus175 <columbia7311@163.com>
2017-01-12 23:42:22 +0800

    2017-01-12 23:45

--- src/PAT_1034.java
+++ src/PAT_1034.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 /**
  * Created by Gavin.Stevenson on 2017/1/12.
  */
 public class PAT_1034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        System.out.println(str1);
+        System.out.println(str2);
+        int a1 = Integer.parseInt( str1.replaceAll("/", " ").split(" ")[0]);
+        int b1 = Integer.parseInt( str1.replaceAll("/", " ").split(" ")[1]);
+        int a2 = Integer.parseInt( str2.replaceAll("/", " ").split(" ")[0]);
+        int b2 = Integer.parseInt( str2.replaceAll("/", " ").split(" ")[1]);
+
+//        String newStr1 = str1.replaceAll("/", " ");
+//        String newStr2 = str2.replaceAll("/", " ");
+//        System.out.println(newStr1);
+//        System.out.println(newStr2 );
+    }
 
 }
