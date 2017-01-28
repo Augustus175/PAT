0cf0e51599a0fdc65cccf9d0c496bf55ffe640b6
ZhangZhibo <columbia>
2017-01-28 22:16:03 +0800

    2017-01-28 22:15

--- src/PAT_1036.java
+++ src/PAT_1036.java
@@ -8,25 +8,22 @@ public class PAT_1036 {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         String str = sc.next();
-        int weigth = (int) Math.rint((double) num / 2);
-        System.out.println(weigth);
-        if (num >= 3 && num <= 20) {
-            for (int i = 0; i < num; i++) {
-                System.out.print(str);
-            }
-            System.out.println();
-            for (int i = 0; i < weigth - 2; i++) {
-                System.out.print(str);
-                for (int j = 0; j < num - 2; j++) {
-                    System.out.print(" ");
-                }
-                System.out.println(str);
-            }
-            for (int i = 0; i < num; i++) {
-                System.out.print(str);
-            }
-//        System.out.println(num);
-//        System.out.println(str);
+        double weigth = (int) Math.ceil((double) num / 2);
+
+        for (int i = 1; i <= num; i++) {
+            System.out.print(str);
         }
+        System.out.println();
+        for (int i = 1; i <= weigth - 2; i++) {
+            System.out.print(str);
+            for (int j = 2; j <= num - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println(str);
+        }
+        for (int i = 1; i <= num; i++) {
+            System.out.print(str);
+        }
+
     }
 }
