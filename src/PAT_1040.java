e3b557b582fdde941c145e1d5a977a2f63ad103f
ZhangZhibo <columbia>
2017-02-04 15:10:23 +0800

    2017-02-04 15:11

--- src/PAT_1040.java
+++ src/PAT_1040.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
 
 /**
- * Created by ZhangZhibo on 2017/2/2.
+ * Created by ZhangZhibo on 2017/2/3.
  */
 public class PAT_1040 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        int Pcount = str.length() - str.replaceAll("P","").length();
-        int Acount = str.length() - str.replaceAll("A","").length();
-        int Tcount = str.length() - str.replaceAll("T","").length();
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i)==80)
-            {
-                Pcount--;
-                if(Acount>0&&Tcount>0)
-                {
-                    count = count + Acount*Tcount;
+            char tmp = str.charAt(i);
+            if (80 == tmp) {
+                for (int j = i + 1; j < str.length(); j++) {
+                    char c = str.charAt(j);
+                    if (65 == c)
+                    {
+                        for (int k = j+1; k < str.length(); k++) {
+                            char t = str.charAt(k);
+                            if (84==t)
+                            {
+                                count++;
+                            }
+                        }
+                    }
                 }
-            }else if(str.charAt(i)==65)
-            {
-                Acount--;
-            }else if(str.charAt(i)==84)
-            {
-                Tcount--;
             }
         }
         System.out.println(count);
